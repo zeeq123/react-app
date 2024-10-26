394d613bfa84f7b682ae2d3f29a8b6664716988c
kjs <zeeq125@gmail.com>
2024-10-26 15:42:20 +0900

    컴포넌트, props 2

--- src/App.js
+++ src/App.js
@@ -10,13 +10,18 @@ function Header(props) {
   );
 }
 
-function Nav() {
+function Nav(props) {
+  const lis = [];
+
+  for (let i = 0; i < props.topics.length; i++) {
+    let t = props.topics[i];
+    lis.push(<li key={t.id}><a href={'/read/' + t.id}>{t.title}</a></li>);
+  }
+
   return (
     <nav>
       <ol>
-        <li><a href="/read/1"></a>html</li>
-        <li><a href="/read/2"></a>css</li>
-        <li><a href="/read/3"></a>js</li>
+        {lis}
       </ol>
     </nav>
   );
@@ -32,10 +37,16 @@ function Article(props) {
 }
 
 function App() {
+  const topics = [
+    { id: 1, title: 'html', body: 'html is...' },
+    { id: 2, title: 'css', body: 'css is...' },
+    { id: 3, title: 'javascript', body: 'javascript is...' }
+  ];
+
   return (
     <div>
       <Header title="WEB"></Header>
-      <Nav></Nav>
+      <Nav topics={topics}></Nav>
       <Article title="Welcome" body="Hello, Web"></Article>
     </div>
   );
