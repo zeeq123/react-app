c63620a87f5492c00695dfef685ab91224d215bd
kjs <zeeq125@gmail.com>
2024-10-26 16:19:04 +0900

    컴포넌트 이벤트

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 
 function Header(props) {
-  console.log('props', props.title);
   return (
     <header>
-      <h1><a href="/">{props.title}</a></h1>
+      <h1><a href="/" onClick={(event) => {
+        event.preventDefault();
+        props.onChangeMode();
+      }}>{props.title}</a></h1>
     </header>
   );
 }
@@ -15,7 +17,10 @@ function Nav(props) {
 
   for (let i = 0; i < props.topics.length; i++) {
     let t = props.topics[i];
-    lis.push(<li key={t.id}><a href={'/read/' + t.id}>{t.title}</a></li>);
+    lis.push(<li key={t.id}><a id={t.id} href={'/read/' + t.id} onClick={(event) => {
+      event.preventDefault();
+      props.onChangeMode(event.target.id);
+    }}>{t.title}</a></li>);
   }
 
   return (
@@ -45,8 +50,12 @@ function App() {
 
   return (
     <div>
-      <Header title="WEB"></Header>
-      <Nav topics={topics}></Nav>
+      <Header title="WEB" onChangeMode={() => {
+        alert('Header');
+      }}></Header>
+      <Nav topics={topics} onChangeMode={(id) => {
+        alert(id);
+      }}></Nav>
       <Article title="Welcome" body="Hello, Web"></Article>
     </div>
   );
