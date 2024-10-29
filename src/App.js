f2afa8674dd14b90e7db4323ba1e219469c25cb7
zeeq125 <zeeq125@gmail.com>
2024-10-29 15:14:18 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
+import React from 'react';
 
+// Header
 function Header(props) {
   return (
     <header>
@@ -13,6 +15,7 @@ function Header(props) {
   );
 }
 
+// Nav
 function Nav(props) {
   const lis = [];
 
@@ -33,6 +36,7 @@ function Nav(props) {
   );
 }
 
+// Article
 function Article(props) {
   return (
     <article>
@@ -42,6 +46,7 @@ function Article(props) {
   );
 }
 
+// Create
 function Create(props) {
   return (
     <article>
@@ -60,6 +65,33 @@ function Create(props) {
   );
 }
 
+// Update
+function Update(props) {
+  const [title, setTitle] = useState(props.title);
+  const [body, setBody] = useState(props.body);
+
+  return (
+    <article>
+      <h2>Update</h2>
+      <form onSubmit={event => {
+        event.preventDefault();
+        const title = event.target.title.value;
+        const body = event.target.body.value;
+        props.onUpdate(title, body);
+      }}>
+        <p><input type='text' name='title' placeholder='title' value={title} onChange={event => {
+          setTitle(event.target.value);
+        }} /></p>
+        <p><textarea name='body' placeholder='body' value={body} onChange={event => {
+          setBody(event.target.value);
+        }}></textarea></p>
+        <p><input type='submit' value='Update' /></p>
+      </form>
+    </article>
+  );
+}
+
+// App
 function App() {
   const [mode, setMode] = useState('WELCOME');
   const [id, setId] = useState(null);
@@ -74,6 +106,7 @@ function App() {
 
 
   let content = null;
+  let contextControl = null;
 
   if (mode === 'WELCOME') {
     content = <Article title="Welcome" body="Hello, Web"></Article>
@@ -86,6 +119,10 @@ function App() {
       }
     }
     content = <Article title={title} body={body}></Article>;
+    contextControl = <li><a href={'/update/' + id} onClick={event => {
+      event.preventDefault();
+      setMode('UPDATE');
+    }}>Update</a></li>;
   } else if (mode === 'CREATE') {
     content = <Create onCreate={(_title, _body) => {
       const newTopic = { id: nextId, title: _title, body: _body };
@@ -96,7 +133,28 @@ function App() {
       setId(nextId);
       setNextId(nextId + 1);
     }}></Create>
+  } else if (mode === 'UPDATE') {
+    let title, body = null;
+    for (let i = 0; i < topics.length; i++) {
+      if (topics[i].id === id) {
+        title = topics[i].title;
+        body = topics[i].body;
+      }
+    }
+    content = <Update title={title} body={body} onUpdate={(title, body) => {
+      const newTopics = [...topics];
+      const updatedTopic = { id: id, title: title, body: body };
+      for (let i = 0; i < topics.length; i++) {
+        if (newTopics[i].id === id) {
+          newTopics[i] = updatedTopic;
+          break;
+        }
+      }
+      setTopics(newTopics);
+      setMode('READ');
+    }}></Update>;
   }
+
 
   return (
     <div>
@@ -108,10 +166,16 @@ function App() {
         setId(_id);
       }}></Nav>
       {content}
-      <a href="/create" onClick={event => {
-        event.preventDefault();
-        setMode('CREATE');
-      }}>Create</a>
+      <ul>
+        <li>
+          <a href="/create" onClick={event => {
+            event.preventDefault();
+            setMode('CREATE');
+          }}>Create</a>
+        </li>
+        {contextControl}
+      </ul>
+
     </div>
   );
 }
